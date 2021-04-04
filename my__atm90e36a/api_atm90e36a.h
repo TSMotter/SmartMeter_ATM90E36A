9a15a9b78b23c212a835cda3fa886597dc709eb9
TSMotter <mottergiuliano@gmail.com>
2021-04-04 18:00:54 -0300

    Altera local do enum  dos tipos de medidas de aquisição

--- my__atm90e36a/api_atm90e36a.h
+++ my__atm90e36a/api_atm90e36a.h
@@ -97,53 +97,6 @@ typedef struct
 	uint16_t	      	Retry;
 }ATM90_app_st;
 
-enum
-{
-	voltage_rms_a = 0,
-	current_rms_a,
-	active_power_a,
-	reactive_power_a,
-	aparent_power_a,
-	active_fundamental_power_a,
-	active_harmonic_power_a,
-	voltage_thdn_a,
-	current_thdn_a,
-	voltage_2_order_harmonic_ratio_a,
-	voltage_3_order_harmonic_ratio_a,
-	voltage_4_order_harmonic_ratio_a,
-	voltage_5_order_harmonic_ratio_a,
-	voltage_thd_a, // phase A, Voltage, Total Harmonic Distortion Ratio
-	current_2_order_harmonic_ratio_a,
-	current_3_order_harmonic_ratio_a,
-	current_4_order_harmonic_ratio_a,
-	current_5_order_harmonic_ratio_a,	
-	current_thd_a, // phase A, Current, Total Harmonic Distortion Ratio
-
-	voltage_rms_b,
-	current_rms_b,
-	active_power_b,
-	reactive_power_b,
-	aparent_power_b,
-	active_fundamental_power_b,
-	active_harmonic_power_b,
-	voltage_thdn_b,
-	current_thdn_b,
-
-	voltage_rms_c,
-	current_rms_c,
-	active_power_c,
-	reactive_power_c,
-	aparent_power_c,
-	active_fundamental_power_c,
-	active_harmonic_power_c,
-	voltage_thdn_c,
-	current_thdn_c,
-
-	frequency,
-
-	numero_total_medidas,
-};
-
 typedef struct
 {
 	bool 	active;
